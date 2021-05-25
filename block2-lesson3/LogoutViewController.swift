c3aca415abf7aad255a5f718e4471716ca2d1d77
Ivan Ch <vano3d@gmail.com>
2021-05-26 01:37:49 +0300

    login & pass is checking, but fields after Logout doesn't clear

--- block2-lesson3/LogoutViewController.swift
+++ block2-lesson3/LogoutViewController.swift
@@ -17,9 +17,33 @@ class LogoutViewController: UIViewController {
         // print something?
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+            self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
     
     @IBAction func tappedLogoutButton() {
          dismiss(animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let mainVC = segue.destination as? ViewController else {
+            return
+        }
+        mainVC.userText = "4"
+        mainVC.passText = "5"
+    }
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard segue.source is LogoutViewController else {
+            return
+        }
+        
     }
     
 }
--- block2-lesson3/ViewController.swift
+++ block2-lesson3/ViewController.swift
@@ -16,22 +16,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var forgotUserNameButton: UIButton!
     @IBOutlet weak var forgotPasswordButton: UIButton!
     
+    var userText = "aaa"
+    var passText = "bbb"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //meybe something 
+        userNameField.text = userText
+        passwordField.text = passText
+        
     }
 
     @IBAction func tappedLoginButton(_ sender: Any) {
-    
+        
+        if userNameField.text == "User" && passwordField.text == "Password" {
+            let logoutVC = self.storyboard?.instantiateViewController(withIdentifier: "logoutStoryboard") as! LogoutViewController
+                    let navigationController = UINavigationController(rootViewController: logoutVC)
+            self.present(navigationController, animated: true, completion: nil)
+        } else {
+            print("wrong!")
+        }
+        
     }
     
     @IBAction func tappedForgotUserNameButton(_ sender: Any) {
-        print("Forgot User")
+        print("Your User name is User :-)")
     }
     
     @IBAction func tappedForgotPasswordButton(_ sender: Any) {
-        print("Forgot Pass")
+        print("Your Passwoer is Password :-)")
     }
     
 }
